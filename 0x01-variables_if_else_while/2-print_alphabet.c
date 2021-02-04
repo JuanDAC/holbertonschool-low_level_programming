f1bec103fcd670a0d89a62dfc6fb76a6123ab676
JuanDAC <juanavila12.0308@gmail.com>
2021-02-04 22:11:27 +0000

    0x01-variables_if_else_while 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,5 @@
-
 #include <stdio.h>
+
 /**
 * main - Entry point
 *
@@ -7,6 +7,15 @@
 */
 int main(void)
 {
+	int i;
+	int lowerCase = 97;
+	int lastCase = 26;
+
+	for (i = 0; i < lastCase; i++)
+	{
+		putchar(lowerCase + i);
+	}
+	putchar('\n');
 	return (0);
 }
 
