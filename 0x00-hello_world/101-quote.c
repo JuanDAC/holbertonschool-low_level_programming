ad98e38183b80b3d53ae4275ca086c26b014a70f
JuanDAC <juanavila12.0308@gmail.com>
2021-02-04 00:27:17 +0000

    2-assembler

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
+#include <unistd.h>
 
 /**
 * main - Entry point
 *
-* Return: Always 0 (Success)
+* Return: Always 1 (Error)
 */
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (1);
 }
 
