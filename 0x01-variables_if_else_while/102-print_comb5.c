2c1e6256e703f56e9080ad48841868d52be1082c
JuanDAC <juanavila12.0308@gmail.com>
2021-02-05 04:53:51 +0000

    last today \(^-^)/

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,5 @@
-
 #include <stdio.h>
+
 /**
 * main - Entry point
 *
@@ -7,6 +7,28 @@
 */
 int main(void)
 {
+	int numberLeft;
+	int numberLast;
+
+	for (numberLeft = 0; numberLeft < 100; numberLeft++)
+	{
+		for (numberLast = 0; numberLast < 100; numberLast++)
+		{
+			if (numberLeft < numberLast)
+			{
+				putchar(numberLeft / 10 + 48);
+				putchar(numberLeft % 10 + 48);
+				putchar(32);
+				putchar(numberLast / 10 + 48);
+				putchar(numberLast % 10 + 48);
+				if (numberLeft < 98)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
-
