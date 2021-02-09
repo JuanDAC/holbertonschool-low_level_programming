53346d3bf23741f5dc983c0fe8bdad9f9627ce9c
JuanDAC <juanavila12.0308@gmail.com>
2021-02-09 16:22:31 +0000

    ./2-alphabet_x10 | issue Betty return

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-* print_alphabet - print the alphabet on screen
+* print_alphabet_x10 - function that prints 10 times the alphabet, in lowercase
 *
 * Return: void
 */
