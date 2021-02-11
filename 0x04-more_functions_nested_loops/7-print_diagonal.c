75e4913346ac41dea147ea9b7eb22a1a67a731ca
JuanDAC <juanavila12.0308@gmail.com>
2021-02-11 21:35:59 +0000

    0x04-more_functions_nested_loops 7 - 8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,13 +1,34 @@
-#include holberton.h
+#include "holberton.h"
 #include <stdio.h>
 /**
-* main - Entry point
-*
+* add_space - print spaces
+* @space: int lines
+*/
+void add_space(int space)
+{
+	if (space > 0)
+	{
+		_putchar(' ');
+		add_space(--space);
+	}
+}
+/**
+* print_diagonal - Entry point
+* @n: int
 * Return: Always 0 (Success)
 */
-int main(void)
+void print_diagonal(int n)
 {
-	
-	return (0);
+	const int TOP = n;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+		for (n = 0; n < TOP; n++)
+		{
+			add_space(n);
+			_putchar('\\');
+			_putchar('\n');
+		}
 }
 
