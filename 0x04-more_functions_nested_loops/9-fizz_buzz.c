52aa588fdb58079377bd58c8e9d6dd5111657ad0
JuanDAC <juanavila12.0308@gmail.com>
2021-02-11 23:29:29 +0000

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,7 @@
-#include holberton.h
+#include "holberton.h"
 #include <stdio.h>
+#define Fizz "Fizz"
+#define Buzz "Buzz"
 /**
 * main - Entry point
 *
@@ -7,7 +9,21 @@
 */
 int main(void)
 {
-	
+	short numbre = 1;
+
+	printf("%d", numbre);
+	while (numbre++ < 100)
+	{
+		if ((numbre % 3) == 0 && (numbre % 5) == 0)
+			printf(" %s%s", Fizz, Buzz);
+		else if ((numbre % 5) == 0)
+			printf(" %s", Buzz);
+		else if ((numbre % 3) == 0)
+			printf(" %s", Fizz);
+		else if ((numbre % 3) && (numbre % 5))
+			printf(" %d", numbre);
+	}
+	printf("\n");
 	return (0);
 }
 
