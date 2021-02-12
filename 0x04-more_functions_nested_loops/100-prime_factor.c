9652f6eec527f68632461962c86775cf43e4a624
JuanDAC <juanavila12.0308@gmail.com>
2021-02-12 01:31:02 +0000

    11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,6 @@
-#include holberton.h
+#include "holberton.h"
 #include <stdio.h>
+#include <math.h>
 /**
 * main - Entry point
 *
@@ -7,7 +8,12 @@
 */
 int main(void)
 {
-	
+	long i, numbre = 612852475143;
+
+	for (i = 2; i < sqrt(numbre); i++)
+		while ((numbre % i) == 0)
+			numbre /= i;
+	printf("%ld\n", numbre);
 	return (0);
 }
 
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,5 @@
+void print_number(int n);
+
 void print_triangle(int size);
 
 void print_square(int size);
