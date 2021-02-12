e7aeeca222fbf29fab87251a104f4980f926b188
JuanDAC <juanavila12.0308@gmail.com>
2021-02-12 05:03:14 +0000

    no alcanse

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,13 +1,58 @@
-#include holberton.h
+#include "holberton.h"
 #include <stdio.h>
 /**
-* main - Entry point
-*
+* _print_first - Entry point
+* @n: number
+* @expo: number to module
 * Return: Always 0 (Success)
 */
-int main(void)
+int _print_first(int n, int expo)
 {
-	
-	return (0);
+	const int CURRENTDIV = (n / 10);
+
+	if (CURRENTDIV > 9)
+		return (_print_first(CURRENTDIV, (10 * expo)));
+	if (CURRENTDIV < 0)
+	{
+		_putchar('-');
+		_putchar((-CURRENTDIV) + '0');
+	}
+	else
+		_putchar((n % 10) + '0');
+	return (expo);
 }
+
+/**
+* _print_number - Entry point
+* @n: number
+* Return: Always 0 (Success)
+*/
+void _print_number(int n)
+{
+	int mod;
+
+	if (n % 10 != 0 && n / 10 != 0)
+	{
+		mod = _print_first(n, 10);
+		if ((n % mod) < (mod / 10))
+			putchar('0');
+		_print_number(n % mod);
+	}
+}
+
+
+/**
+* print_number - Entry point
+* @n: number
+* Return: Always 0 (Success)
+*/
+void print_number(int n)
+{
+	if (n % 10 == 0 && n / 10 == 0)
+		_putchar('0');
+	else
+		_print_number(n);
+	_putchar('\n');
+}
+
 
