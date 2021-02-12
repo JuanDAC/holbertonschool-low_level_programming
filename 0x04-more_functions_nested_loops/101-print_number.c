7d1114016a7f8d61c8ebc9e8c8c2dce4a1aad393
JuanDAC <juanavila12.0308@gmail.com>
2021-02-12 05:23:43 +0000

    ya casi

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,58 +1,32 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-* _print_first - Entry point
-* @n: number
-* @expo: number to module
-* Return: Always 0 (Success)
-*/
-int _print_first(int n, int expo)
-{
-	const int CURRENTDIV = (n / 10);
-
-	if (CURRENTDIV > 9)
-		return (_print_first(CURRENTDIV, (10 * expo)));
-	if (CURRENTDIV < 0)
-	{
-		_putchar('-');
-		_putchar((-CURRENTDIV) + '0');
-	}
-	else
-		_putchar((n % 10) + '0');
-	return (expo);
-}
-
-/**
-* _print_number - Entry point
-* @n: number
-* Return: Always 0 (Success)
-*/
-void _print_number(int n)
-{
-	int mod;
-
-	if (n % 10 != 0 && n / 10 != 0)
-	{
-		mod = _print_first(n, 10);
-		if ((n % mod) < (mod / 10))
-			putchar('0');
-		_print_number(n % mod);
-	}
-}
-
-
-/**
 * print_number - Entry point
 * @n: number
 * Return: Always 0 (Success)
 */
 void print_number(int n)
 {
-	if (n % 10 == 0 && n / 10 == 0)
+	int length = 10;
+	int number = n;
+
+	if (n == 0)
 		_putchar('0');
 	else
-		_print_number(n);
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			n = (-n);
+		}
+		for (; n > 0; n /= 1000)
+			length *= 10;
+		for (n = length; n > 0; n /= 10)
+		{
+			if (!(n == length && ((number / n) % 10) == 0))
+				_putchar('0' + (number / n) % 10);
+		}
+	}
+
 	_putchar('\n');
 }
-
-
