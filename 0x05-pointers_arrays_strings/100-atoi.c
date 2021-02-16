6676b71be2370647a932a348db2138b6d1ec1ae6
JuanDAC <juanavila12.0308@gmail.com>
2021-02-16 15:40:08 +0000

    [atoi.c] (working with main)

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -26,18 +26,26 @@ int _atoi(char *s)
 
 	for (pos = length; pos >= 0; pos--)
 	{
+		if (*(s + pos) == '-' && exp > 1)
+			sign *= -1;
+		else if (*(s + pos) == '+' && exp > 1)
+			sign *= 1;
 		for (iter = 0; iter <= 9; iter++)
 		{
 			if (*(s + pos) == ('0' + iter))
 			{
 
-				if (((pos + 1) <= length) && !(*(s + (pos + 1)) >= '0' && *(s + (pos + 1)) < '9'))
+				if (
+					((pos + 1) <= length)
+					&& !(
+						*(s + (pos + 1)) >= '0'
+						&& *(s + (pos + 1)) < '9'
+					)
+				)
 				{
 					exp = 1;
 					exit = 0;
 				}
-				if (((pos - 1) >= 0) && *(s + (pos - 1)) == '-')
-					sign = -1;
 				exit += (iter * exp);
 				exp *= 10;
 			}
