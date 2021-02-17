ad48955975a31cdc2fd9d09c9ce538b2ab0bb8b1
JuanDAC <juanavila12.0308@gmail.com>
2021-02-17 14:58:34 +0000

    [keygen.c] (create)

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,44 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * createPassword - generates random valid passwords
- * @key: passwords
- * @n: iteration
- */
-void createPassword(char *key, int n)
-{
-	int r;
-
-	srand(time(NULL));
-	if (n < 2645)
-	{
-		r = rand() % 122;
-		if (r > 32)
-		{
-			*key = r;
-			key += 1;
-			n += r;
-		}
-		createPassword(key, n);
-	}
-	else
-	{
-		*key = (2772 - n);
-		*(key + 1) = '\n';
-	}
-
-}
-/**
  * main - random valid passwords for the program 101-crackme.
  * Return: always 0
  */
 int main(void)
 {
-	char a[100];
-	int r, n;
+	char resoul[200];
+	int number = 0, random = 0;
+	char *key = resoul;
 
-	srand(time(NULL));
-	createPassword(a, 0);
-	printf("%s", a);
+	while (number < 2645)
+	{
+		random = rand() % 122;
+		if (random > 32)
+		{
+			*key = random;
+			key = key + 1;
+			number += random;
+		}
+	}
+	*key = (2772 - number);
+	*(key + 1) = '\n';
+	printf("%s", resoul);
 	return (0);
 }
