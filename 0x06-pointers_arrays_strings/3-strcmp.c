7eeda01c908684a6caac72de843b5e1182af466a
JuanDAC <juanavila12.0308@gmail.com>
2021-02-18 23:51:55 +0000

    [3-strcmp.c] (issue resuelto gracias a Jerson Perez all characters)

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,6 +8,13 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	return (((int) *s1) - ((int) *s2));
+	int operation;
+
+	do {
+		operation = (int) (*s1 - *s2);
+		s1++;
+		s2++;
+	} while (operation == 0 && (*s1 != '\0' && *s2 != '\0'));
+	return (operation);
 }
 
