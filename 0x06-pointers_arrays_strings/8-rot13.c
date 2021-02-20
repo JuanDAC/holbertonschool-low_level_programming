3bd8c722f3dcfbc7f49e164fec4c78a262286bce
JuanDAC <juanavila12.0308@gmail.com>
2021-02-20 21:58:24 +0000

    [0x06 ~ 8-rot13.c] (create)

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,13 +1,21 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-* main - Entry point
-* @ :
+* rot13 - Entry point
+* @string: pointer - string
 * Return: Always 0 (Success)
 */
-int main(void)
+char *rot13(char *string)
 {
-	
-	return (0);
+	char (*alphabet) = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char (*ROT13) = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; *(string + i) != '\0'; i++)
+		for (j = 0; *(alphabet + j) != '\0'; j++)
+			if (*(alphabet + j) == *(string + i))
+				*(string + i) = *(ROT13 + j);
+
+	return (string);
 }
 
