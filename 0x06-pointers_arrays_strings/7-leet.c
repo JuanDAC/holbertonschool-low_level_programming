34272d0a363862dc260d4de61ea342f1bbf34234
JuanDAC <juanavila12.0308@gmail.com>
2021-02-20 20:48:33 +0000

    [0x06 ~ 7-leet.c] (clear code)

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,12 +8,22 @@
 */
 char *leet(char *string)
 {
-	int i;
+	char (*letters) = "AEOTL",
+	(*numbers) = "43071",
+	current_upper,
+	current_lower,
+	current_letter;
+	int i, j;
 
 	for (i = 0; *(string + i) != '\0'; i++)
-		if (hash(string + i))
-		{}
-
+		for (j = 0; *(letters + j) != '\0'; j++)
+		{
+			current_upper = *(letters + j);
+			current_lower = *(letters + j) + 32;
+			current_letter = *(string + i);
+			if (current_letter == current_upper || current_letter == current_lower)
+				*(string + i) = *(numbers + j);
+		}
 	return (string);
 }
 
