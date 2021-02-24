8cb6b252a56600d8f60249c0f9024996ec800241
JuanDAC <juanavila12.0308@gmail.com>
2021-02-24 22:53:21 +0000

    [0x08 ~ 100-wildcmp.c] (create)

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,13 +1,26 @@
-#include holberton.h
+#include "holberton.h"
 #include <stdio.h>
 /**
-* main - Entry point
-* @ :
+* wildcmp - Entry point
+* @s1: char
+* @s2: char
 * Return: Always 0 (Success)
 */
-int main(void)
+int wildcmp(char *s1, char *s2)
 {
-	
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s2 == *s1)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 != '*')
+		return (0);
+	if (*s2 == '\0' && *s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
 	return (0);
 }
 
