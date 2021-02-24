ba1689ae6fe23050fceb7e5c6eaef6bda62bc889
JuanDAC <juanavila12.0308@gmail.com>
2021-02-24 17:07:40 +0000

    [0x08 ~ README.md] (create)

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,17 @@
-#include holberton.h
+#include "holberton.h"
 #include <stdio.h>
 /**
-* main - Entry point
-* @ :
+* _pow_recursion -  point
+* @x: char
+* @y: char
 * Return: Always 0 (Success)
 */
-int main(void)
+int _pow_recursion(int x, int y)
 {
-	
-	return (0);
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
 
