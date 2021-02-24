215737075c9356e197c5d167b8207ccbc1d4fad8
JuanDAC <juanavila12.0308@gmail.com>
2021-02-24 18:55:01 +0000

    [0x08 ~ 5-sqrt_recursion.c] (create)

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,32 @@
 #include "holberton.h"
 #include <stdio.h>
+double _sqrt(double x, double y)
+{
+	double div = (x / y) - y;
+
+	if (div < 0)
+		div *= -1;
+	if (div < .001)
+		return (y);
+	return (_sqrt(x, ((y + x / y) / 2)));
+}
 /**
-* _pow_recursion -  point
-* @x: char
-* @y: char
+* _sqrt_recursion -  point
+* @n: char
 * Return: Always 0 (Success)
 */
-int _pow_recursion(int x, int y)
+int _sqrt_recursion(int n)
 {
-	if (y < 0)
+	double r;
+
+	if (n < 0)
 		return (-1);
-	if (y == 0)
+	if (n == 0)
 		return (1);
-	return (x * _pow_recursion(x, y - 1));
+	r = _sqrt((double) n, (double) 1);
+	if ((double)(r - (int)r) > 0.1)
+		return (-1);
+
+	return ((int)(r));
 }
 
