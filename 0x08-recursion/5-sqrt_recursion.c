4f40a3887434db4c839bb968896fa0c87639d22e
JuanDAC <juanavila12.0308@gmail.com>
2021-02-24 18:58:31 +0000

    [0x08 ~ 5-sqrt_recursion.c] (fix set range to .001)

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,7 +24,7 @@ int _sqrt_recursion(int n)
 	if (n == 0)
 		return (1);
 	r = _sqrt((double) n, (double) 1);
-	if ((double)(r - (int)r) > 0.1)
+	if ((double)(r - (int)r) > .001)
 		return (-1);
 
 	return ((int)(r));
