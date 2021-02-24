55fca26f85404df1750cd3df532c506db39cb634
JuanDAC <juanavila12.0308@gmail.com>
2021-02-24 20:01:53 +0000

    [0x08 ~ 6-is_prime_number.c] (create)

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,29 @@
-#include holberton.h
+#include "holberton.h"
 #include <stdio.h>
 /**
-* main - Entry point
-* @ :
+* prime - point
+* @n: double
+* @divisor: double
 * Return: Always 0 (Success)
 */
-int main(void)
+int prime(int n, int divisor)
 {
-	
-	return (0);
+	if (n / 2 < divisor)
+		return (1);
+	else if (n % divisor == 0)
+		return (0);
+	return (prime(n, divisor + 1));
+
+}
+/**
+* is_prime_number -  point
+* @n: char
+* Return: Always 0 (Success)
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime(n, 2));
 }
 
