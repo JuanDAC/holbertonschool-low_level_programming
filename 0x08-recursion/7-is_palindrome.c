42d4811e4cd336c7dee4031949f1ee68a28e0968
JuanDAC <juanavila12.0308@gmail.com>
2021-02-24 20:43:29 +0000

    [0x08 ~ 7-palindrome.c] (create)

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,13 +1,41 @@
-#include holberton.h
+#include "holberton.h"
 #include <stdio.h>
 /**
-* main - Entry point
-* @ :
+* length - Entry point
+* @s: char
 * Return: Always 0 (Success)
 */
-int main(void)
+int length(char *s)
 {
-	
-	return (0);
+	if (*s == '\0')
+		return (0);
+	return (1 + length(s + 1));
+}
+
+/**
+* palindrome - Entry point
+* @s: char
+* @l: char
+* @i: char
+* Return: Always 0 (Success)
+*/
+int palindrome(char *s, int l, int i)
+{
+	if ((l / 2) == i)
+		return (1);
+	return (*(s + i) == *(s + (l - 1 - i)) && palindrome(s, l, i + 1));
+}
+/**
+* is_palindrome - Entry point
+* @s: char
+* Return: Always 0 (Success)
+*/
+int is_palindrome(char *s)
+{
+	int l = length(s);
+
+	if (l == 0)
+		return (1);
+	return (palindrome(s, l, 0));
 }
 
