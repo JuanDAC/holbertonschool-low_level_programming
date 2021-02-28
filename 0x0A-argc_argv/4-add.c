3c76192f3fd0657479a9258c1aabf92c95b8bd74
JuanDAC <juanavila12.0308@gmail.com>
2021-02-28 17:38:57 +0000

    [0x0A ~ 4-add.c] (update: change to my file)

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,71 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * is_zero - adds positive numbers
- * @string: string to validate
- *
- * Return: Always 0
- */
-int is_zero(char *string)
+* _pow - Entry point
+* @base: int
+* @expo: int
+* Return: Always 0 (Success)
+*/
+int _pow(int base, int expo)
+{
+	return ((expo == 0) ? 1 : (base * _pow(base, expo - 1)));
+}
+/**
+* length - Entry point
+* @string: int
+* Return: Always 0 (Success)
+*/
+int length(char *string)
+{
+	return ((*string == '\0') ? 0 : 1 + length(string + 1));
+}
+/**
+* paserInt - Entry point
+* @string: int
+* Return: Always 0 (Success)
+*/
+int paserInt(char *string)
+{
+	int number_raw, processed_number;
+
+	if (*string == '\0')
+		return (0);
+
+	number_raw = ((int)*string) - 48;
+	processed_number = number_raw * _pow(10, length(string)) / 10;
+	return (processed_number + paserInt(string + 1));
+}
+/**
+* is_number - Entry point
+* @string: int
+* Return: Always 0 (Success)
+*/
+int is_number(char *string)
 {
 	if (*string == '\0')
 		return (1);
-	return (
-		((*string >= '0' && *string <= '9') || *string == '-')
-		&& is_zero(string + 1)
-	);
+	return ((*string >= '0' && *string <= '9') && is_number(string + 1));
 }
 /**
- * main - adds positive numbers
- * @argc: counter of arguments
- * @argv: vector of strings in arguments
- *
- * Return: Always 0
- */
+* main - Entry point
+* @argc: int
+* @argv: int
+* Return: Always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) == 0 && !is_zero(argv[i]))
+		if (!is_number(*(argv + i)))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[i]);
+		sum += paserInt(*(argv + i));
 	}
 	printf("%d\n", sum);
+
 	return (0);
 }
 
---
+++ 0x0A-argc_argv/4-add_adriana.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * is_zero - adds positive numbers
+ * @string: string to validate
+ *
+ * Return: Always 0
+ */
+int is_zero(char *string)
+{
+	if (*string == '\0')
+		return (1);
+	return (
+		((*string >= '0' && *string <= '9') || *string == '-')
+		&& is_zero(string + 1)
+	);
+}
+/**
+ * main - adds positive numbers
+ * @argc: counter of arguments
+ * @argv: vector of strings in arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0 && !is_zero(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
