166ce9d2710b01131e557b1e8ff13c2231883217
JuanDAC <juanavila12.0308@gmail.com>
2021-03-01 01:00:13 +0000

    [0x0A ~ 100-change.c] (fix -> type: print, = to +=)

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,31 +9,29 @@
 */
 int main(int argc, char *argv[])
 {
-	int long coins, cents;
+	int long coins = 0, cents = 0;
 
 	if (argc != 2)
 	{
-		print("ERORR\n");
+		printf("ERORR\n");
 		return (EXIT_FAILURE);
 	}
-	if (atoi(*(argv + 1)) <= 0)
-		cents = 0;
-	else
+	if (atoi(*(argv + 1)) > 0)
 		cents = atoi(*(argv + 1));
 	if (cents > 0)
 	{
-		coins = cents / 25;
+		coins += cents / 25;
 		cents %= 25;
-		coins = cents / 10;
+		coins += cents / 10;
 		cents %= 10;
-		coins = cents / 5;
+		coins += cents / 5;
 		cents %= 5;
-		coins = cents / 2;
+		coins += cents / 2;
 		cents %= 2;
-		coins = cents / 1;
+		coins += cents / 1;
 		cents %= 1;
 	}
-	print("%d\n", coins);
+	printf("%ld\n", coins);
 	return (EXIT_SUCCESS);
 }
 
