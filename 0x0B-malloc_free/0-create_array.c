9b84d003862466f88b6eb18b338936f7cb56e3d0
JuanDAC <juanavila12.0308@gmail.com>
2021-03-02 21:44:21 +0000

    [0x0B ~ 0-create_array.c] (fix -> Check [5-12]: when c is null)

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-	if (size == 0)
+	if (size == 0 || c == '\0')
 		return (NULL);
 	array = (char *)malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
