be033bcc982c5287be924069dada3cb62dbaf796
JuanDAC <juanavila12.0308@gmail.com>
2021-03-02 21:19:55 +0000

    [0x0B ~ 0-create_array.c] (fix -> Check [5-12]: remake whithout recurtion)

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,21 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* array_fill - create a array than custom size and
-*                fill c character
-* @array: charater to fill array
-* @c: charater to fill array
-* @size: size of array
-* Return: Pointer to array or null if size is 0
-*/
-char *array_fill(char *array, unsigned int size, char c)
-{
-	if (size == 0)
-		return (array);
-	*(array + size - 1) = c;
-	return (array_fill(array, size - 1, c));
-}
-/**
 * create_array - create a array than custom size and
 *                fill c character
 * @c: charater to fill array
@@ -25,12 +10,15 @@ char *array_fill(char *array, unsigned int size, char c)
 */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+
 	if (size == 0)
 		return (NULL);
 	char *array = (char *)malloc(size * sizeof(char));
 
+	for (i = 0; i < size; i++)
+		*(array + i) = c;
 
-
-	return (array_fill(array, size, c));
+	return (array);
 }
 
