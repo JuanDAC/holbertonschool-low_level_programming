ec6d8c7b6156e10d94b79164fdfa6ad8226d3b13
JuanDAC <juanavila12.0308@gmail.com>
2021-03-03 00:30:42 +0000

    [0x0B ~ 2-str_concat.c] (fix: declarate on top)

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,6 +10,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int i, j, length_s1 = 0, length_s2 = 0;
+	char *str;
 
 	if (s1 == NULL)
 		*s1 = '\0';
@@ -18,15 +19,17 @@ char *str_concat(char *s1, char *s2)
 
 	length_s1 = sizeof(s1);
 	length_s2 = sizeof(s2);
-	char *str = malloc(sizeof(char) * (length_s1 + length_s2 - 1));
+	str = malloc(sizeof(char) * (length_s1 + length_s2 - 1));
 
 	if (str == NULL)
 		return (NULL);
 
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		*(str + i) = *(s1 + i);
+
 	for (j = 0; *(s2 + j) != '\0'; i++, j++)
 		*(str + i) = *(s2 + j);
+
 	*(str + i) = *(s2 + j);
 	return (str);
 }
