281346b0c37595441b8994364a1f44ffd6d9cb50
JuanDAC <juanavila12.0308@gmail.com>
2021-03-03 00:18:45 +0000

    [0x0B ~ 2-str_concat.c] (fix: add sizeof  )

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int i, j, length_s1 = 0, length_s2 = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	length_s1 = sizeof(s1);
 	length_s2 = sizeof(s2);
