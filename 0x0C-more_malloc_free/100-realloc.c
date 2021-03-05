63ebc1927222c8be0c075920f6edf6902894705a
JuanDAC <juanavila12.0308@gmail.com>
2021-03-05 02:32:33 +0000

    [0x0C ~ 100-realloc.c] (create)

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,13 +1,34 @@
-#include holberton.h
+#include "holberton.h"
 #include <stdio.h>
 /**
-* main - Entry point
-* @ :
+* _realloc - Entry point
+* @ptr: void *
+* @old_size: unsigned int
+* @new_size: unsigned int
 * Return: Always 0 (Success)
 */
-int main(void)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	
-	return (0);
-}
+	void *stream;
+	int i;
 
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	stream = malloc(new_size);
+	if (stream == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size; i++)
+		*(stream + i) = *(ptr + i);
+
+	free(ptr);
+
+	return (stream);
+}
