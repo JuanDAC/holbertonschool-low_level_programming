db855645958c402bbe31734787410f71282f6198
JuanDAC <juanavila12.0308@gmail.com>
2021-03-08 22:50:27 +0000

    [0x0E ~ 2-print_dog.c] (create)

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,13 +1,17 @@
-#include holberton.h
 #include <stdio.h>
+#include "dog.h"
 /**
-* main - Entry point
-* @ :
+* print_dog - Entry point
+* @d: struct dog *
 * Return: Always 0 (Success)
 */
-int main(void)
+void print_dog(struct dog *d)
 {
-	
-	return (0);
+	if (d == NULL)
+		return;
+	else if (d->name == NULL || d->name == NULL || d->owner == NULL)
+		printf("(nil)");
+	else
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
 
