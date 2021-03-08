6380f223f965f48eb27535e87cbd8aa6891ca414
JuanDAC <juanavila12.0308@gmail.com>
2021-03-08 23:18:02 +0000

    [0x0E ~ 2-print_dog.c] (Check 6 -> Code fail: define NILL)

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+#define NILL "(nill)"
 /**
 * print_dog - Entry point
 * @d: struct dog *
@@ -9,11 +10,12 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	else if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else if (d->name == NULL)
-		printf("Name: (nil)\n");
 	else
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf(
+			"Name: %s\nAge: %f\nOwner: %s\n",
+			(d->name == NULL) ? NILL : d->name ,
+			d->age,
+			(d->owner == NULL) ? NILL : d->name
+		);
 }
 
