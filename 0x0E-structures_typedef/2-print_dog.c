31b8a2f8ddaf1d9723a1ee0385e7bc51154f5836
JuanDAC <juanavila12.0308@gmail.com>
2021-03-08 22:54:41 +0000

    [0x0E ~ 2-print_dog.c] (Check 6 -> Code fail)

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	else if (d->name == NULL || d->name == NULL || d->owner == NULL)
+	else if (d->name == NULL || d->age == 0 || d->owner == NULL)
 		printf("(nil)");
 	else
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
