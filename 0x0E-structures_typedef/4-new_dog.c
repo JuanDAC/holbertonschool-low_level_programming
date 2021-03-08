6a7a1f39c98361918ea5738b6845c7c73dc1e2f9
JuanDAC <juanavila12.0308@gmail.com>
2021-03-08 23:42:41 +0000

    [0x0E ~ 4-new_dog.c] (create)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,24 @@
-#include holberton.h
+#include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
-* main - Entry point
-* @ :
-* Return: Always 0 (Success)
+* new_dog - Entry point
+* @name: char
+* @age: char
+* @owner: char
+* Return: dog_t
 */
-int main(void)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	
-	return (0);
+	dog_t *my_new_dog;
+
+	my_new_dog = malloc(sizeof(dog_t));
+	if (my_new_dog == NULL)
+		return (NULL);
+	my_new_dog->name = name;
+	my_new_dog->age = age;
+	my_new_dog->owner = owner;
+
+	return (my_new_dog);
 }
 
