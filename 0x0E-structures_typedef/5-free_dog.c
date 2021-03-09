8c49d375a16532896518dc42fbced21a93600aca
JuanDAC <juanavila12.0308@gmail.com>
2021-03-09 01:46:06 +0000

    [0x0E ~ 5-free_dog.c] (free)

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,12 +7,11 @@
 */
 void free_dog(dog_t *d)
 {
-	if (d->name != NULL)
-		free(d->name);
-	if (d->owner != NULL)
-		free(d->owner);
-	if (d != NULL)
-		free(d);
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
 	return (0);
 }
 
