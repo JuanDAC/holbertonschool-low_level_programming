6bce28e9890e27d92f88417a3d2475c94602e7e1
JuanDAC <juanavila12.0308@gmail.com>
2021-03-10 18:21:00 +0000

    [0x0F ~ 2-int_index.c] (create)

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,13 +1,21 @@
-#include holberton.h
 #include <stdio.h>
+#include "function_pointers.h"
 /**
-* main - Entry point
-* @ :
+* int_index - Entry point
+* @array: a array
+* @size: a array
+* @cmp: a array
 * Return: Always 0 (Success)
 */
-int main(void)
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	
-	return (0);
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(*(array + i)))
+			return (i);
+	return (-1);
 }
 
