e3d236efa5751cfb4e7734c0f86448adbefe5611
JuanDAC <juanavila12.0308@gmail.com>
2021-03-11 21:53:47 +0000

    [0x10 ~ 1-print_numbers.c] (fix: delete exit to NULL print separator)

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,8 +14,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numbers;
 	unsigned int i;
 
-	if (separator == NULL)
-		return;
 	va_start(numbers, n);
 	for (i = 0; i < n; i++)
 	{
