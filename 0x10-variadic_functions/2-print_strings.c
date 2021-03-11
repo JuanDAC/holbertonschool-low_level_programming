3c9c0651b40f52602d5388a9b927259f48ae0799
JuanDAC <juanavila12.0308@gmail.com>
2021-03-11 22:00:05 +0000

    [0x10 ~ 2-print_strings.c] (fix: delete exit to NULL separator)

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define NILL "(nil)"
 
 /**
 * print_strings - Entry point
@@ -15,15 +16,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *current_string;
 
-	if (separator == NULL)
-		return;
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
 		current_string = va_arg(arg, char *);
-		current_string = current_string ? current_string : "(nil)";
-		separator = ((i + 1) == n) ? ("") : separator;
-		printf("%s%s", current_string, separator);
+		current_string = current_string ? current_string : NILL;
+		printf("%s", current_string);
+		if (separator != NULL && (i + 1) != n)
+			printf("%s", separator);
 	}
 	va_end(arg);
 	printf("\n");
