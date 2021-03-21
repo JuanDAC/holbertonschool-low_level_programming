3f44e69a0955b201d23d62e71609b7fbee005762
JuanDAC <juanavila12.0308@gmail.com>
2021-03-21 22:32:38 +0000

    [0x12 ~ assembly] (from 32 to 64)

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,20 @@
 section	.data
 format		db	"%s, %s",10,0
-message1	db	"Holberton",0
-message2	db	"Hello",0
+message2	db	"Holberton",0
+message1	db	"Hello",0
 
 	section	.text
 	extern	printf
 	global	main
 
 main:
-	mov		edx,	message1
-	mov		esi,	message2
-	mov		edi,	format
-	mov		eax,	0
+	mov		rdx,	message2
+	mov		rsi,	message1
+	mov		rdi,	format
+	mov		rax,	0
 	call	printf
 
-	mov		ebx,	0
-	mov		eax,	1
+	mov		rbx,	0
+	mov		rax,	1
 	int		0x80
 
