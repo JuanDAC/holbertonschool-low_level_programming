570ed19bc266985e6797887f3675138a73675d7d
JuanDAC <juanavila12.0308@gmail.com>
2021-03-23 19:20:12 +0000

    [0x13 ~ 5-free_listint2.c] (re make)

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,19 @@
 */
 void free_listint2(listint_t **head)
 {
+	listint_t *current_node;
+	listint_t *free_node;
+
 	if (*head == NULL || head == NULL)
 		return;
+	current_node = *head;
+	while (current_node != NULL)
+	{
+		free_node = current_node;
+		current_node = current_node->next;
+		free(free_node);
+	}
 
-	free_listint2(&(*head)->next);
-	free(*head);
 	*head = NULL;
 }
 
