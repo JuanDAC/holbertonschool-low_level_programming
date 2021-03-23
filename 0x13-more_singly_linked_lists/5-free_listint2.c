9252bdb0081f594a67b2f6952601f35f8922ee61
JuanDAC <juanavila12.0308@gmail.com>
2021-03-23 16:16:19 +0000

    [0x13 ~ 5-free_listint2.c] (delete another function)

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,27 +1,17 @@
 #include "lists.h"
 
 /**
-* free_listint - Entry point
-* @head: current node
-* Return: Always 0 (Success)
-*/
-void free_listint(listint_t *head)
-{
-	if (head == NULL)
-		return;
-
-	free_listint(head->next);
-	free(head);
-}
-
-/**
 * free_listint2 - Entry point
 * @head: current node
 * Return: Always 0 (Success)
 */
 void free_listint2(listint_t **head)
 {
-	free_listint(*head);
+	if (head == NULL)
+		return;
+
+	free_listint2(&(*head)->next);
+	free(*head);
 	*head = NULL;
 }
 
