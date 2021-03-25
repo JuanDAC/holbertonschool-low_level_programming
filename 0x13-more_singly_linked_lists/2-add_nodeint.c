4e259e6a7ab13f85428e35acca2b99ba0b37d82a
JuanDAC <juanavila12.0308@gmail.com>
2021-03-25 03:14:20 +0000

    [0x13 ~ 2-add_nodeint.c] (fix: delete conditional NULL)

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,15 +10,15 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *current_new_node;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	current_new_node = malloc(sizeof(listint_t));
 	if (current_new_node == NULL)
 		return (NULL);
 
-	current_new_node->next = *head;
 	current_new_node->n = n;
+	current_new_node->next = *head;
 	*head = current_new_node;
 	return (*head);
 }
