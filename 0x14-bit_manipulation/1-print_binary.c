7ee1204b9d2e1e646c550f3eccc8f37127ed78d7
JuanDAC <juanavila12.0308@gmail.com>
2021-03-26 01:46:31 +0000

    [0x14 ~ 1-print_binary.c] (fix: not use arrays)

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,21 +1,25 @@
+
 #include "holberton.h"
 #define MAX_BINARY 36
 /**
 * binary - Entry point
-* @buffer: buffer string
+* @start_binary: start_binary
 * @number: number
 * @index: index
 * Return: Always 0 (Success)
 */
-void binary(char *buffer, unsigned long int number, int index)
+void binary(bool *start_binary, unsigned long int number, int index)
 {
-	if (index == 0)
+	if (index == MAX_BINARY)
 		return;
 
-	*(buffer + index) = (number & 0x1) + '0';
+	binary(start_binary, number >> 1, index + 1);
 
-	_putchar(*(buffer + index));
-	binary(buffer, number >> 1, index - 1);
+	if (!*start_binary && (number & 0x1) + '0' == '1')
+		*start_binary = true;
+
+	if (*start_binary)
+		_putchar((number & 0x1) + '0');
 }
 /**
 * print_binary - Entry point
@@ -24,20 +28,10 @@ void binary(char *buffer, unsigned long int number, int index)
 */
 void print_binary(unsigned long int n)
 {
-	char buffer[MAX_BINARY];
-	unsigned int i;
 	bool start_binary = false;
 
 	if (n == 0)
 		_putchar('0');
 
-	binary(buffer, n, MAX_BINARY - 1);
-
-	for (i = 1; i < MAX_BINARY; i++)
-	{
-		if (!start_binary && buffer[i] == '1')
-			start_binary = true;
-		if (start_binary)
-			_putchar(buffer[i]);
-	}
+	binary(&start_binary, n, 0);
 }
