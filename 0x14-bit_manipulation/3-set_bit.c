2c0e92abc9bb4b8c88011ab8573e941f94b10c4c
JuanDAC <juanavila12.0308@gmail.com>
2021-03-26 04:29:57 +0000

    [0x13 ~ 3-set_bit.c] (refactor: clear code)

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,32 +1,4 @@
 #include "holberton.h"
-#define MAX_BINARY 80
-/**
-* binary_length - Entry point
-* @start_binary: start_binary
-* @number: number
-* @index: index
-* Return: Always 0 (Success)
-*/
-size_t binary_length(
-	bool *start_binary,
-	unsigned long int number,
-	unsigned long int index
-)
-{
-	size_t idx;
-
-	if (index == MAX_BINARY)
-		return (0);
-
-	idx = binary_length(start_binary, number >> 1, index + 1);
-
-	if (!*start_binary && (number & 0x1) + '0' == '1')
-		*start_binary = true;
-
-	if (*start_binary)
-		return (1 + idx);
-	return (0);
-}
 /**
 * set_bit - Entry point
 * @n: number to comvert binary
