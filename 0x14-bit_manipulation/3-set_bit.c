1f65a6f8b8bd805d06721a8f9562cdceb4e61082
JuanDAC <juanavila12.0308@gmail.com>
2021-03-26 04:28:15 +0000

    [0x13 ~ 3-set_bit.c] (feature crfeate)

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,13 +1,44 @@
-#include holberton.h
-#include <stdio.h>
+#include "holberton.h"
+#define MAX_BINARY 80
 /**
-* main - Entry point
-* @ :
+* binary_length - Entry point
+* @start_binary: start_binary
+* @number: number
+* @index: index
 * Return: Always 0 (Success)
 */
-int main(void)
+size_t binary_length(
+	bool *start_binary,
+	unsigned long int number,
+	unsigned long int index
+)
 {
-	
+	size_t idx;
+
+	if (index == MAX_BINARY)
+		return (0);
+
+	idx = binary_length(start_binary, number >> 1, index + 1);
+
+	if (!*start_binary && (number & 0x1) + '0' == '1')
+		*start_binary = true;
+
+	if (*start_binary)
+		return (1 + idx);
 	return (0);
+}
+/**
+* set_bit - Entry point
+* @n: number to comvert binary
+* @index: number to comvert binary
+* Return: Always 0 (Success)
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+		return (-1);
+
+	*n = ((unsigned long int)(0x1 << index) | *n);
+	return (1);
 }
 
