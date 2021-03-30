1c8b0bb7166d7aa6e349bcc74d4b20e9aec49313
JuanDAC <juanavila12.0308@gmail.com>
2021-03-30 03:30:51 +0000

    [0x15 ~ 2-append_text_to_file.c] (create)

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,46 @@
-#include holberton.h
-#include <stdio.h>
+#include "holberton.h"
+
 /**
-* main - Entry point
-* @ :
-* Return: Always 0 (Success)
-*/
-int main(void)
+  * length_str - function that creates a file.
+  * @string: string
+  * Return: get length of string
+  */
+size_t length_str(const char *string)
 {
-	
-	return (0);
+	return ((*string == '\0') ? 0 : 1 + length_str(string + 1));
 }
+/**
+* append_text_to_file - function that appends text at the end of a file.
+* @filename: filename
+* @text_content: text_content
+* Return: size
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_id = 0;
+	size_t size;
+
+	if (!filename)
+	return (-1);
+
+	file_id = open(filename, O_APPEND | O_WRONLY, 0600);
+	if (file_id == -1)
+		return (-1);
+
+	size = length_str(text_content);
+
+	if (write(file_id, text_content, size) == -1)
+		return (-1);
+
+	close(file_d);
+	return (1);
+}
+
+
+
+
+
+
+
+
 
