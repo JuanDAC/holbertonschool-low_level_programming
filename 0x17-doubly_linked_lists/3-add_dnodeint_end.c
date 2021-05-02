ed06e04de2d774d9c0c50384adb747b766d84e35
JuanDAC <juanavila12.0308@gmail.com>
2021-05-02 03:06:15 +0000

    (0x17 ~ 3-add_dnodeint_end.c) [create ]

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -23,7 +23,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		(*head)->next = new_node;
 		new_node->prev = (*head);
 		new_node->next = NULL;
-		return (new_node);
+		return ((*head)->next);
 	}
 
 	return (add_dnodeint_end(&(*head)->next, n));
