ad6c85d63b5152b747b4db8da742cbf366364e26
JuanDAC <juanavila12.0308@gmail.com>
2021-05-02 02:59:39 +0000

    (0x17 ~ 3-add_dnodeint_end.c) [create]

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,13 +1,31 @@
-#include holberton.h
+#include "lists.h"
 #include <stdio.h>
 /**
-* main - Entry point
-* @ :
+* add_dnodeint_end - Entry point
+* @head: node
+* @n: node
 * Return: Always 0 (Success)
 */
-int main(void)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	
-	return (0);
+	dlistint_t *new_node;
+
+	if (!head || !*head)
+		return (NULL);
+
+	if ((*head)->next == NULL)
+	{
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node)
+			return (NULL);
+
+		new_node->n = n;
+		(*head)->next = new_node;
+		new_node->prev = (*head);
+		new_node->next = NULL;
+		return (new_node);
+	}
+
+	return (add_dnodeint_end(&(*head)->next, n));
 }
 
