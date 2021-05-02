611fc07934621c90051caaadbd9d36dab3f38a2e
JuanDAC <juanavila12.0308@gmail.com>
2021-05-02 03:21:47 +0000

    (0x17 ~ 3-add_dnodeint_end.c) [fix handler error malloc]

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,20 +10,23 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
-	if (!head || !*head)
+	if (!head)
 		return (NULL);
 
-	if ((*head)->next == NULL)
+	if (!*head || (*head)->next == NULL)
 	{
 		new_node = malloc(sizeof(dlistint_t));
-		if (new_node)
+		if (!new_node)
 			return (NULL);
 
 		new_node->n = n;
-		(*head)->next = new_node;
+		if (*head)
+			(*head)->next = new_node;
+		else
+			*head = new_node;
 		new_node->prev = (*head);
 		new_node->next = NULL;
-		return ((*head)->next);
+		return (new_node);
 	}
 
 	return (add_dnodeint_end(&(*head)->next, n));
