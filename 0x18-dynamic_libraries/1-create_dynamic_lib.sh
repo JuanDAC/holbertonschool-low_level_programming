a94d7f9e517bf02e6157a5e5136701f4b7e7ff26
JuanDAC <juanavila12.0308@gmail.com>
2021-05-03 19:27:04 +0000

    (0x18 - 1-create_dynamic_lib.sh) [fix: remove extra]

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-gcc -fPIC -Wall -Werror -Wextra -pedantic -c *.c
-gcc *.c -c -fPIC
-gcc *.o -shared -o libholberton.so
+gcc -fPIC -Wall -Werror -Wextra -pedantic -c ./*.c
+gcc ./*.c -c -fPIC
+gcc ./*.o -shared -o liball.so
 
 ls -la lib*
-nm -D libholberton.so
+nm -D liball.so
 
-gcc -Wall -pedantic -Werror -Wextra -L. 0-main.c -lholberton -o len
-
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
-
-ldd len
-rm *.o
+rm ./*.o
