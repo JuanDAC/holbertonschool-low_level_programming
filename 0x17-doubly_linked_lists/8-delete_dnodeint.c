c9832d2d53bde37190de8e8c2b0b76ad71cf32e5
JuanDAC <juanavila12.0308@gmail.com>
2021-05-03 02:00:22 +0000

    [0x17 ~ 8-delete_dnodeint.c] (doc: betty)

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,27 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * create_node - make node and define pointer to NULL
- * @new_node: pointer to the variable storage the pointer to new node
- * @n: value or data to storage in node
- * Return: new node or NULL if fail
- */
-int create_node(dlistint_t **new_node, int n)
-{
-	*new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (0);
-	(*new_node)->n = n;
-	(*new_node)->next = NULL;
-	(*new_node)->prev = NULL;
-	return (1);
-}
-
-/**
-* insert_dnodeint_at_index - Entry point
-* @h: head
-* @idx: index
-* @n: index
+* delete_dnodeint_at_index - Entry point
+* @head: head
+* @index: index
 * Return: Always 0 (Success)
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
