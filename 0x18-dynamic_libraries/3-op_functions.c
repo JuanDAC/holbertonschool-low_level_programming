c0b199e142fa3169ce5c1d1b89c673321aff667d
JuanDAC <juanavila12.0308@gmail.com>
2021-05-04 02:18:53 +0000

    lib python3

---
+++ 0x18-dynamic_libraries/3-op_functions.c
@@ -0,0 +1,56 @@
+
+/**
+* add - op add
+* @a: number
+* @b: number
+* Return: soult operation
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - op add
+* @a: number
+* @b: number
+* Return: soult operation
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - op add
+* @a: number
+* @b: number
+* Return: soult operation
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - op add
+* @a: number
+* @b: number
+* Return: soult operation
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - op add
+* @a: number
+* @b: number
+* Return: soult operation
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
