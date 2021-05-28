1132b222960e3e71fe03f75e39dcf2d451c6180d
JuanDAC <juanavila12.0308@gmail.com>
2021-05-28 09:59:01 -0500

    [0x1A ~ Update] (change sizeof)

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,22 +1,22 @@
  #include "hash_tables.h"
 
 /**
- * hash_table_create - create a hash table
+ * hash_table_create - Create a hash table
  *
- * @size: size of the array the hash table
+ * @size: Size of the array the hash table
  *
- * Return: return the structure of hash table
+ * Return: the structure of hash table
  *
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
 
-	hash_table = malloc(sizeof(hash_table_t));
+	hash_table = malloc(sizeof(*hash_table));
 	if (!hash_table)
 		return (NULL);
 	hash_table->size = size;
-	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	hash_table->array = calloc(size, sizeof(*(hash_table->array)));
 	if (!hash_table->array)
 	{
 		free(hash_table);
