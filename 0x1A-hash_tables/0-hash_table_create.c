ed0abc23b2832a5dfdce0a653055e9d94b3df7a4
JuanDAC <juanavila12.0308@gmail.com>
2021-05-28 10:20:06 -0500

    [0x1A ~ fix](sizeof to type)

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,12 +12,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
 
-	hash_table = malloc(sizeof(*hash_table));
-	if (!hash_table)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
 	hash_table->size = size;
-	hash_table->array = calloc(size, sizeof(*(hash_table->array)));
-	if (!hash_table->array)
+	hash_table->array = calloc(hash_table->size, sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
