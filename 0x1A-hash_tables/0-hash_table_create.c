f382b5aa984e78df852cee21698dded4acd8c91d
JuanDAC <juanavila12.0308@gmail.com>
2021-05-28 11:30:36 -0500

    [0x1A ~ fix] (Creata a variable)

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,22 +5,26 @@
  *
  * @size: Size of the array the hash table
  *
+ *
  * Return: the structure of hash table
  *
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
+	hash_node_t **array = NULL;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
 	hash_table->size = size;
-	hash_table->array = calloc(hash_table->size, sizeof(hash_node_t *));
-	if (hash_table->array == NULL)
+
+	array = calloc(size, sizeof(hash_node_t *));
+	if (array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
 	}
+	hash_table->array = array;
 	return (hash_table);
 }
