39366269f5497e093cfdacffb42c97b2659afc53
JuanDAC <juanavila12.0308@gmail.com>
2021-05-28 22:01:23 -0500

    [0x1A ~ fix] (The key previous free)

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,17 +1,5 @@
 #include "hash_tables.h"
 
-#define create_a_node(NEW_NODE, VALUE, KEY, INDEX, HASH_TABLE)\
-do {                                                          \
-	NEW_NODE = malloc(sizeof(hash_node_t));                   \
-	if (NEW_NODE)                                             \
-	{                                                         \
-		NEW_NODE->value = VALUE;                              \
-		NEW_NODE->key = KEY;                                  \
-		NEW_NODE->next = HASH_TABLE->array[INDEX];            \
-		HASH_TABLE->array[index] = NEW_NODE;                  \
-	}                                                         \
-} while (0)
-
 /**
  * navegate_to_node_match - navegate to node match
  *
@@ -45,7 +33,6 @@ int navegate_to_node_match(hash_node_t *current, const char *key)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	char *copy_value = 0, *copy_key = 0;
 	hash_node_t *new_node = NULL, *current = NULL;
 
 	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
@@ -53,34 +40,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	copy_value = calloc(strlen(value) + 1, sizeof(unsigned char));
-	if (!copy_value)
-		return (0);
-	strcpy(copy_value, value);
-
-	copy_key = calloc(strlen(key) + 1, sizeof(unsigned char));
-	if (!copy_key)
-	{
-		free(copy_value);
-		return (0);
-	}
 	current = (hash_node_t *)ht->array[index];
 	if (navegate_to_node_match(current, key))
 	{
 		free(current->value);
-		free(copy_key);
-		current->value = copy_value;
+		current->value = strdup(value);
 	}
 	else
 	{
-		strcpy(copy_key, key);
-		create_a_node(new_node, copy_value, copy_key, index, ht);
+		new_node = malloc(sizeof(hash_node_t));
 		if (!new_node)
-		{
-			free(copy_key);
-			free(copy_value);
 			return (0);
-		}
+		new_node->value = strdup(value);
+		new_node->key = strdup(key);
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
 	}
 	return (0);
 }
