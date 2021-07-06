f784154b9030db46fc4efc9a660bd2b7b26e71de
JuanDAC <juanavila12.0308@gmail.com>
2021-07-06 06:44:10 -0400

    [docs] (add module documentetion)

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Function that returns the perimeter of the island described in grid
+"""
+
 
 def island_perimeter(grid):
     '''find permiter'''
@@ -11,8 +15,8 @@ def island_perimeter(grid):
     for row in range(1, size - 1):
         for col in range(1, size - 1):
             if (grid[row][col]):
-                    perimeter += grid[row][col - 1] == 0
-                    perimeter += grid[row - 1][col] == 0
-                    perimeter += grid[row][col + 1] == 0
-                    perimeter += grid[row + 1][col] == 0
+                perimeter += grid[row][col - 1] == 0
+                perimeter += grid[row - 1][col] == 0
+                perimeter += grid[row][col + 1] == 0
+                perimeter += grid[row + 1][col] == 0
     return (perimeter)
