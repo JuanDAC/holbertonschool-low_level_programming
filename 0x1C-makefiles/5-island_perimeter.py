9d228b8b5a5391622d6f93a11ac9c97c94da24f5
JuanDAC <juanavila12.0308@gmail.com>
2021-07-06 06:53:21 -0400

    [fix] (column less thant 3)

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,10 +13,13 @@ def island_perimeter(grid):
         return 0
 
     for row in range(1, size - 1):
+        if (len(grid[row]) <= 2):
+            continue
         for col in range(1, size - 1):
             if (grid[row][col]):
                 perimeter += grid[row][col - 1] == 0
                 perimeter += grid[row - 1][col] == 0
                 perimeter += grid[row][col + 1] == 0
                 perimeter += grid[row + 1][col] == 0
-    return (perimeter)
+
+    return perimeter
